d444529db9036a597efe10b84b070647e5be71b8
Edward Dijk <edward@enso.nl>
2024-04-15 14:26:25 +0200

    Added gitignore files and folders.

--- prereq.sh
+++ prereq.sh
@@ -3,6 +3,7 @@
 echo "Downloading prerequisited files.."
 
 # Sources to be downloaded
-virtio_exe="https://fedorapeople.org/groups/virt/virtio-win/direct-downloads/latest-virtio/virtio-win-guest-tools.exe"
+virtio_exe_url="https://fedorapeople.org/groups/virt/virtio-win/direct-downloads/latest-virtio/virtio-win-guest-tools.exe"
+virtio_exe_filename="build_files/software/virtio-win-guest-tools.exe"
 
-
+curl -L $virtio_exe_url --output $virtio_exe_filename
